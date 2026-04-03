702c500037ebdb1b2ee16b7c82c339812e7e2790
Mary-Samuela <samuelamary@gmail.com>
2026-04-03 22:13:06 +0300

    insert deployment values

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 // ── Middleware ──
 app.use(
   cors({
-    origin: "http://localhost:5173", // your React app
+    origin: ["http://localhost:5173", "https://catholic-backend.onrender.com"],
     credentials: true,
   }),
 );
